a748dc711a827bacc3b100998293cff8ffdde82f
nmhung03 <manhhung3072003@gmail.com>
2025-03-07 17:48:10 +0700

    kết nối Sequelize đến database + điều chỉnh server

--- Nodejs/src/config/connectDB.js
+++ Nodejs/src/config/connectDB.js
@@ -5,3 +5,14 @@ const sequelize = new Sequelize('test', 'root', null, {
   host: 'localhost',
   dialect: 'mysql'
 });
+
+let connectDB = async() => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+
+module.exports = connectDB;
