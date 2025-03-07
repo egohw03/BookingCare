ad8560816334891fe0be48f8c0336397021d6c59
nmhung03 <manhhung3072003@gmail.com>
2025-03-07 18:03:03 +0700

    lấy data từ db

--- Nodejs/src/controllers/homeController.js
+++ Nodejs/src/controllers/homeController.js
@@ -3,10 +3,10 @@ import db from "../models/index.js";
 let getHomePage = async (req, res) => {
     try {
         let data = await db.User.findAll();
-        console.log('--------------');
-        console.log(data);
-        console.log('--------------');
-        return res.render("homepage.ejs");
+
+        return res.render("homepage.ejs", {
+            data: JSON.stringify(data)
+        });
     } catch (error) {
         console.log(error);
     }
