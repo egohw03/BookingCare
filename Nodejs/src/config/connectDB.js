5d519748b0d16d4ce790b7549c1fd78f43dd6de8
nmhung03 <manhhung3072003@gmail.com>
2025-03-16 14:54:00 +0700

    update config connectDB

--- Nodejs/src/config/connectDB.js
+++ Nodejs/src/config/connectDB.js
@@ -1,10 +1,10 @@
 const { Sequelize } = require('sequelize');
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('test', 'root', null, {
-  host: 'localhost',
-  dialect: 'mysql',
-  logging: false
+const sequelize = new Sequelize('bookingcare', 'root', null, {
+    host: 'localhost',
+    dialect: 'mysql',
+    logging: false
 });
 
 let connectDB = async() => {
