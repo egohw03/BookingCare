347f0c92641fccffb5243225d8243eed86196ca3
nmhung03 <manhhung3072003@gmail.com>
2025-04-04 16:32:31 +0700

    dùng Redux để chuyển đổi ngôn ngữ

--- Reactjs/src/containers/HomePage/HomeHeader.js
+++ Reactjs/src/containers/HomePage/HomeHeader.js
@@ -3,9 +3,15 @@ import { connect } from 'react-redux';
 import './HomeHeader.scss';
 import logo from '../../assets/logo.svg';
 import { FormattedMessage } from 'react-intl';
-class HomeHeader extends Component {
+import { LANGUAGES } from '../../utils';
 
+import { changeLanguageApp } from '../../store/actions';
+class HomeHeader extends Component {
+    changeLanguage = (language) => {
+        this.props.changeLanguageAppRedux(language);
+    }
     render() {
+        let language = this.props.language;
         return (
             <React.Fragment>
                 <div className="home-header-container">
@@ -34,8 +40,18 @@ class HomeHeader extends Component {
                         </div>
                         <div className="right-content">
                             <div className="support"><i className="fas fa-question-circle"></i> <FormattedMessage id="homeheader.support" /></div>
-                            <div className="language-vi">VN</div>
-                            <div className="language-en">EN</div>
+                            <div className={
+                                language === LANGUAGES.VI ? 'language-vi active' : 'language-vi'}>
+                                <span onClick={() => this.changeLanguage(LANGUAGES.VI)}>
+                                    VN
+                                </span>
+                            </div>
+                            <div className={
+                                language === LANGUAGES.EN ? 'language-en active' : 'language-en'}>
+                                <span onClick={() => this.changeLanguage(LANGUAGES.EN)}>
+                                    EN
+                                </span>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -85,12 +101,14 @@ class HomeHeader extends Component {
 
 const mapStateToProps = state => {
     return {
-        isLoggedIn: state.user.isLoggedIn
+        isLoggedIn: state.user.isLoggedIn,
+        language: state.app.language
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
+        changeLanguageAppRedux: (language) => dispatch(changeLanguageApp(language))
     };
 };
 
