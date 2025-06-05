bc50a6fe645c9d24d93863253c1db5089d969a53
nmhung03 <manhhung3072003@gmail.com>
2025-06-06 01:50:34 +0700

    hoàn tất base giao diện

---
+++ Reactjs/src/containers/HomePage/Section/About.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class About extends Component {
+    render() {
+        return (
+            <div className='section-share section-about'>
+                <div className='section-about-header'>
+                    truyền thông
+                </div>
+                <div className='section-about-content'>
+                    <div className='content-left'>
+                        <iframe
+                            width="560" height="315"
+                            src="https://www.youtube.com/embed/J6NWaNtQEHA"
+                            title="YouTube video player"
+                            frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            allowfullscreen></iframe>
+
+                    </div>
+                    <div className='content-right'>
+                        <div>
+                            Bốc trúng Secret
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        isLoggedIn: state.user.isLoggedIn,
+        language: state.app.language,
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(About);
